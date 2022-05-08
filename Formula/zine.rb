a45478349c352a1cfa621e32019193cf647da3e0
Folyd <lyshuhow@gmail.com>
2022-05-08 23:52:49 +0800

    Fix ruby syntax error

--- Formula/zine.rb
+++ Formula/zine.rb
@@ -1,11 +1,11 @@
 class Zine < Formula
-    desc "Zine - a simple and opinionated tool to build your own magazine."
-    homepage "https://github.com/zineland/zine"
-    url "https://github.com/zineland/zine/releases/download/v0.4.0/zine-macos-amd64"
-    sha256 "216724d4a50ea2033cfa980311ee00cb4f881246639a2a70c8881688cfe9516b"
-    version "0.4.0"
+  desc "Zine - a simple and opinionated tool to build your own magazine."
+  homepage "https://github.com/zineland/zine"
+  url "https://github.com/zineland/zine/releases/download/v0.4.0/zine-macos-amd64"
+  version "0.4.0"
+  sha256 "216724d4a50ea2033cfa980311ee00cb4f881246639a2a70c8881688cfe9516b"
   
-    def install
-      bin.install "zine"
-    end
+  def install
+    bin.install "zine"
   end
+end
