8576a38dfb711fbecccfa3b59b887b83653826a6
Folyd <lyshuhow@gmail.com>
2022-05-09 00:38:03 +0800

    Fix lint

--- Formula/zine.rb
+++ Formula/zine.rb
@@ -2,7 +2,7 @@ class Zine < Formula
   desc "Simple and opinionated tool to build your own magazine"
   homepage "https://github.com/zineland/zine"
   version "0.4.0"
-  
+
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/zineland/zine/releases/download/v0.4.0/zine-macos-amd64"
     sha256 "216724d4a50ea2033cfa980311ee00cb4f881246639a2a70c8881688cfe9516b"
