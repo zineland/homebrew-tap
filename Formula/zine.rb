2453bdd86d09564e6110b79a7c3002f3eede555e
Folyd <lyshuhow@gmail.com>
2022-05-08 23:56:07 +0800

    Fix lints

--- Formula/zine.rb
+++ Formula/zine.rb
@@ -1,10 +1,10 @@
 class Zine < Formula
-  desc "Zine - a simple and opinionated tool to build your own magazine."
+  desc "A simple and opinionated tool to build your own magazine"
   homepage "https://github.com/zineland/zine"
   url "https://github.com/zineland/zine/releases/download/v0.4.0/zine-macos-amd64"
   version "0.4.0"
   sha256 "216724d4a50ea2033cfa980311ee00cb4f881246639a2a70c8881688cfe9516b"
-  
+
   def install
     bin.install "zine"
   end
