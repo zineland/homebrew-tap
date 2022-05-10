9c8333a672deeffb354665f48496abe20f508ee6
Folyd <lyshuhow@gmail.com>
2022-05-10 23:46:33 +0800

    Update urls

--- Formula/zine.rb
+++ Formula/zine.rb
@@ -4,12 +4,12 @@ class Zine < Formula
   version "0.4.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/zineland/zine/releases/download/v0.4.0/zine-macos-amd64"
+    url "https://github.com/zineland/zine/releases/download/v0.4.1/zine-x86_64-apple-darwin.tar.gz"
     sha256 "216724d4a50ea2033cfa980311ee00cb4f881246639a2a70c8881688cfe9516b"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/zineland/zine/releases/download/v0.4.0/zine-linux-amd64"
+    url "https://github.com/zineland/zine/releases/download/v0.4.1/zine-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "aef942b301f1bff9430aadf8b9d59d985664cee869bfb110a1d555ce41f0c701"
   end
 
