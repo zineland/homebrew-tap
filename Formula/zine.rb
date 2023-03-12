d2eee6be9e934e8304a402767792efce3b3acb0f
Folyd <Folyd@users.noreply.github.com>
2023-03-12 16:53:38 +0000

    Bump zine to v0.13.0

--- Formula/zine.rb
+++ Formula/zine.rb
@@ -1,16 +1,16 @@
 class Zine < Formula
   desc "Simple and opinionated tool to build your own magazine"
   homepage "https://github.com/zineland/zine"
-  version "0.12.1"
+  version "0.13.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/zineland/zine/releases/download/v0.12.1/zine-x86_64-apple-darwin.tar.gz"
-    sha256 "a153a000bb82a268bd24d5fb872748078ee2f40122aae4a4a5173839295b4cf0"
+    url "https://github.com/zineland/zine/releases/download/v0.13.0/zine-x86_64-apple-darwin.tar.gz"
+    sha256 "f354971fc27de64497825630b51501fc83aa07f3b55bd761a7f961db2e2f939f"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/zineland/zine/releases/download/v0.12.1/zine-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "8b234731d62e64f982a2b7a321b2af100be2e145e8a1582856e52953ace2e31d"
+    url "https://github.com/zineland/zine/releases/download/v0.13.0/zine-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "4f56b71edc2c3949c1be281e265b4db0d2e2868f24eba438a0ff1c00fa45bec5"
   end
 
   def install
